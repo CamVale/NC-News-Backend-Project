e9c1acd1847fab9fab771a21cd4b1aa15a275d86
CamVale <cameronvale1@gmail.com>
2023-11-24 12:48:19 +0000

    cleans up code

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -52,7 +52,7 @@ describe("GET /api/articles/:article_id", () => {
           title: "Sony Vaio; or, The Laptop",
           topic: "mitch",
           author: "icellusedkars",
-          body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+          body: expect.any(String),
           created_at: convertTimestampToDate(1602828180000),
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
@@ -223,7 +223,6 @@ describe('GET /api/articles/:article_id/comments', () => {
         expect(response.body.comments.length).toBe(0);
       });
   });
-});
 
 describe("GET /api/articles", () => {
   test("should return status code 200, and an array of all article objects, with a comment_count key included, and body key omitted", () => {
@@ -283,6 +282,9 @@ describe("GET /api/users", () => {
           expect(typeof user.name).toBe("string");
           expect(typeof user.avatar_url).toBe("string");
         });
+      })
+    })
+  })
 
 
 describe('PATCH /api/articles/:article_id', () => {
