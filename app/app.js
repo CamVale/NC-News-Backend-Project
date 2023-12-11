0899ba735bf715c7172e445c47c83a76dc4b8d0b
CamVale <cameronvale1@gmail.com>
2023-12-11 11:33:05 +0000

    allow cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -3,10 +3,12 @@ const express = require('express');
 
 const { getTopics, getEndpoints, getArticlesByID, getArticles, getCommentsByArticleID, postComment, deleteCommentByID, patchVotes, getUsers, getArticlesByTopic, getArticlesByQuery } = require('../controllers/app.controller');
 
-
+const cors = require('cors')
 
 const { handleSQLError, handleCustomErrors } = require('../errors/app.errors');
 const app = express();
+
+app.use(cors())
 
 app.use(express.json())
 
